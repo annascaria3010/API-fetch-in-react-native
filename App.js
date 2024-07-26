16d0d526f2ab2187a97bc0fb33ada9b898b271eb
ANNA SCARIA PALLIVATHUKAL <annascaria>
2024-07-27 01:03:14 +0530

    created edit options

--- App.js
+++ App.js
@@ -20,6 +20,8 @@ export default function App() {
   const [response, setResponse] = useState([]);
   const [showProducts, setShowProducts] = useState(false);
   const [selected, setSelected] = useState(null);
+  const [isAddingProduct, setIsAddingProduct] = useState(false);
+  const [isEditingProduct, setIsEditingProduct] = useState(null);
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
   const [rating, setRating] = useState('');
@@ -59,6 +61,8 @@ export default function App() {
   const handleHomePress = () => {
     setShowProducts(false);
     setSelected(null);
+    setIsAddingProduct(false);
+    setIsEditingProduct(null);
   };
 
   const handleSingleSelection = (getCurrentId) => {
@@ -66,14 +70,18 @@ export default function App() {
   };
 
   const handleAddProduct = () => {
+    setIsAddingProduct(true);
+  };
+
+  const handleSaveProduct = () => {
     const newProduct = {
-      title,
+      title: title,
       price: parseFloat(price),
       rating: {
         rate: parseFloat(rating),
         count: 1,
       },
-      image,
+      image: image,
     };
 
     fetch('https://fakestoreapi.com/products', {
@@ -91,6 +99,49 @@ export default function App() {
           setPrice('');
           setRating('');
           setImage('');
+          setShowProducts(true);
+          setIsAddingProduct(false);
+        },
+        (error) => {
+          setError(error);
+        }
+      );
+  };
+
+  const handleEditProduct = (product) => {
+    setIsEditingProduct(product.id);
+    setTitle(product.title);
+    setPrice(product.price.toString());
+    setRating(product.rating.rate.toString());
+    setImage(product.image);
+  };
+
+  const handleUpdateProduct = () => {
+    const updatedProduct = {
+      title: title,
+      price: parseFloat(price),
+      description: 'Updated description',
+      image: image,
+      category: 'electronic',
+    };
+
+    fetch(`https://fakestoreapi.com/products/${isEditingProduct}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedProduct),
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setResponse(response.map(product => (product.id === isEditingProduct ? result : product)));
+          setTitle('');
+          setPrice('');
+          setRating('');
+          setImage('');
+          setIsEditingProduct(null);
+          setShowProducts(true);
         },
         (error) => {
           setError(error);
@@ -103,7 +154,6 @@ export default function App() {
       method: 'DELETE',
     })
       .then(() => {
-        // Filter out the deleted product from local state
         setResponse(response.filter(product => product.id !== id));
         if (selected === id) {
           setSelected(null);
@@ -129,6 +179,12 @@ export default function App() {
             onPress={() => handleDeleteProduct(item.id)}
           >
             <Text style={styles.deleteButtonText}>Delete</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.editButton}
+            onPress={() => handleEditProduct(item)}
+          >
+            <Text style={styles.editButtonText}>Edit</Text>
           </TouchableOpacity>
         </View>
       ) : null}
@@ -180,37 +236,51 @@ export default function App() {
             <TouchableOpacity style={styles.button} onPress={handlePress}>
               <Text style={styles.buttonText}>View Products</Text>
             </TouchableOpacity>
-            <View style={styles.inputContainer}>
-              <TextInput
-                style={styles.input}
-                placeholder="Title"
-                value={title}
-                onChangeText={setTitle}
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Price"
-                value={price}
-                onChangeText={setPrice}
-                keyboardType="numeric"
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Rating"
-                value={rating}
-                onChangeText={setRating}
-                keyboardType="numeric"
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Image URL"
-                value={image}
-                onChangeText={setImage}
-              />
+            {!isAddingProduct && !isEditingProduct && (
               <TouchableOpacity style={styles.button} onPress={handleAddProduct}>
                 <Text style={styles.buttonText}>Add Product</Text>
               </TouchableOpacity>
-            </View>
+            )}
+            {(isAddingProduct || isEditingProduct) && (
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Title"
+                  value={title}
+                  onChangeText={setTitle}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Price"
+                  value={price}
+                  onChangeText={setPrice}
+                  keyboardType="numeric"
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Rating"
+                  value={rating}
+                  onChangeText={setRating}
+                  keyboardType="numeric"
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Image URL"
+                  value={image}
+                  onChangeText={setImage}
+                />
+                {isAddingProduct && (
+                  <TouchableOpacity style={styles.button} onPress={handleSaveProduct}>
+                    <Text style={styles.buttonText}>Save</Text>
+                  </TouchableOpacity>
+                )}
+                {isEditingProduct && (
+                  <TouchableOpacity style={styles.button} onPress={handleUpdateProduct}>
+                    <Text style={styles.buttonText}>Update</Text>
+                  </TouchableOpacity>
+                )}
+              </View>
+            )}
           </>
         )}
         {getContent()}
@@ -277,6 +347,7 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   inputContainer: {
     width: '80%',
@@ -298,6 +369,18 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   deleteButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  editButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 6,
+    marginTop: 10,
+  },
+  editButtonText: {
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
